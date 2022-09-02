672382cb57a75cb00314595fbe0b8f1d38d23775
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-09-02 09:01:44 +0300

    Add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const router = require("./routes");
 const { errorHandler } = require("./middlewares/error.handler.mw");
 
 const app = express();
-app.use(express.static('public'));
+app.use(cors());
+app.use(express.static("public"));
 app.use(express.json());
 app.use("/api", router);
 app.use(errorHandler);
